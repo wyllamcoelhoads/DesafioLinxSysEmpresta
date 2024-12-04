23a862258c0e6397c9d45b12cc54cf31c1f0ec6e
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2024-12-03 22:57:56 -0300

    adicionando o arquivo de inicialização do initializer

---
+++ sys-empresta/src/main/java/com/systemEmpresta/sys_empresta/SysEmprestaApplication.java
@@ -0,0 +1,13 @@
+package com.systemEmpresta.sys_empresta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysEmprestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysEmprestaApplication.class, args);
+	}
+
+}
---
+++ sys-empresta/src/test/java/com/systemEmpresta/sys_empresta/SysEmprestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemEmpresta.sys_empresta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysEmprestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
